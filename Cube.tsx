7e87402b2c948f50dfb79501d9a8713889911c81
Xvezda <xvezda@naver.com>
2024-01-01 05:40:14 +0900

    fix: correct mis rotation

--- Cube.tsx
+++ Cube.tsx
@@ -17,31 +17,72 @@ export interface Cube {
 
 export function Cube({ data }: { data: Cube }) {
   return (
-    <div>
-      <Face data={data.up} />
-      <Face data={data.front} />
-      <Face data={data.left} />
-      <Face data={data.back} />
-      <Face data={data.right} />
-      <Face data={data.down} />
+    <div
+      style={{
+        width: 150,
+        height: 150,
+        position: "relative",
+        transformStyle: "preserve-3d",
+        transform: "translateX(150px) translateY(150px) rotateX(-45deg) rotateY(-45deg)",
+      }}
+    >
+      <Face
+        data={data.up}
+        style={{ transform: `rotateX(90deg) translateZ(75px)` }}
+      />
+      <Face data={data.front} style={{ transform: `translateZ(75px)` }} />
+      <Face
+        data={data.left}
+        style={{ transform: `rotateY(-90deg) translateZ(75px)` }}
+      />
+      <Face
+        data={data.back}
+        style={{ transform: `rotateY(180deg) translateZ(75px)` }}
+      />
+      <Face
+        data={data.right}
+        style={{ transform: `rotateY(90deg) translateZ(75px)` }}
+      />
+      <Face
+        data={data.down}
+        style={{ transform: `rotateX(-90deg) translateZ(75px)` }}
+      />
+
+      {/** hidden faces clone */}
+      <Face
+        data={data.left}
+        style={{ transform: `rotateY(-90deg) translateZ(225px)` }}
+      />
+      <Face
+        data={data.back}
+        style={{ transform: `rotateY(180deg) translateZ(225px)` }}
+      />
+      <Face
+        data={data.down}
+        style={{ transform: `rotateX(-90deg) translateZ(300px)` }}
+      />
     </div>
   );
 }
 
-export function Face({ data }: { data: Face }) {
+export function Face({ data, style }: { data: Face; style?: any }) {
   return (
-    <div style={{ marginTop: 10 }}>
+    <div
+      style={{ position: "absolute", backfaceVisibility: "visible", ...style }}
+    >
       {data.map((row, i) => (
-        <div key={i} style={{ display: 'flex' }}>
+        <div key={i} style={{ display: "flex" }}>
           {row.map((color, j) => (
             <div
-              key={[i, j, color].join(':')}
+              key={[i, j, color].join(":")}
               style={{
                 display: "flex",
                 width: 50,
                 height: 50,
                 border: "1px solid black",
                 backgroundColor: color,
+                boxSizing: 'border-box',
+                opacity: 0.9,
               }}
             ></div>
           ))}
--- use-cube.ts
+++ use-cube.ts
@@ -62,9 +62,9 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
         ...cube,
         left: spinClockwise(cube.left),
         up: [
-          [cube.back[0][0], cube.up[0][1], cube.up[0][2]],
-          [cube.back[1][0], cube.up[1][1], cube.up[1][2]],
-          [cube.back[2][0], cube.up[2][1], cube.up[2][2]],
+          [cube.back[2][2], cube.up[0][1], cube.up[0][2]],
+          [cube.back[1][2], cube.up[1][1], cube.up[1][2]],
+          [cube.back[0][2], cube.up[2][1], cube.up[2][2]],
         ],
         front: [
           [cube.up[0][0], cube.front[0][1], cube.front[0][2]],
@@ -77,9 +77,9 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
           [cube.front[2][0], cube.down[2][1], cube.down[2][2]],
         ],
         back: [
-          [cube.down[0][0], cube.back[0][1], cube.back[0][2]],
-          [cube.down[1][0], cube.back[1][1], cube.back[1][2]],
-          [cube.down[2][0], cube.back[2][1], cube.back[2][2]],
+          [cube.back[0][0], cube.back[0][1], cube.down[2][0]],
+          [cube.back[1][0], cube.back[1][1], cube.down[1][0]],
+          [cube.back[2][0], cube.back[2][1], cube.down[0][0]],
         ],
       };
     case "L'":
@@ -97,14 +97,14 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
           [cube.down[2][0], cube.front[2][1], cube.front[2][2]],
         ],
         down: [
-          [cube.back[0][0], cube.down[0][1], cube.down[0][2]],
-          [cube.back[1][0], cube.down[1][1], cube.down[1][2]],
-          [cube.back[2][0], cube.down[2][1], cube.down[2][2]],
+          [cube.back[0][2], cube.down[0][1], cube.down[0][2]],
+          [cube.back[1][2], cube.down[1][1], cube.down[1][2]],
+          [cube.back[2][2], cube.down[2][1], cube.down[2][2]],
         ],
         back: [
-          [cube.up[0][0], cube.back[0][1], cube.back[0][2]],
-          [cube.up[1][0], cube.back[1][1], cube.back[1][2]],
-          [cube.up[2][0], cube.back[2][1], cube.back[2][2]],
+          [cube.back[0][0], cube.back[0][1], cube.up[2][0]],
+          [cube.back[1][0], cube.back[1][1], cube.up[1][0]],
+          [cube.back[2][0], cube.back[2][1], cube.up[0][0]],
         ],
       };
     case 'R':
@@ -122,14 +122,14 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
           [cube.front[2][0], cube.front[2][1], cube.down[2][2]],
         ],
         down: [
-          [cube.down[0][0], cube.down[0][1], cube.back[0][2]],
-          [cube.down[1][0], cube.down[1][1], cube.back[1][2]],
-          [cube.down[2][0], cube.down[2][1], cube.back[2][2]],
+          [cube.down[0][0], cube.down[0][1], cube.back[2][0]],
+          [cube.down[1][0], cube.down[1][1], cube.back[1][0]],
+          [cube.down[2][0], cube.down[2][1], cube.back[0][0]],
         ],
         back: [
-          [cube.back[0][0], cube.back[0][1], cube.up[0][2]],
-          [cube.back[1][0], cube.back[1][1], cube.up[1][2]],
-          [cube.back[2][0], cube.back[2][1], cube.up[2][2]],
+          [cube.up[2][2], cube.back[0][1], cube.back[0][2]],
+          [cube.up[1][2], cube.back[1][1], cube.back[1][2]],
+          [cube.up[0][2], cube.back[2][1], cube.back[2][2]],
         ],
       };
     case "R'":
@@ -137,9 +137,9 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
         ...cube,
         right: spinCounterClockwise(cube.right),
         up: [
-          [cube.up[0][0], cube.up[0][1], cube.back[0][2]],
-          [cube.up[1][0], cube.up[1][1], cube.back[1][2]],
-          [cube.up[2][0], cube.up[2][1], cube.back[2][2]],
+          [cube.up[0][0], cube.up[0][1], cube.back[2][0]],
+          [cube.up[1][0], cube.up[1][1], cube.back[1][0]],
+          [cube.up[2][0], cube.up[2][1], cube.back[0][0]],
         ],
         front: [
           [cube.front[0][0], cube.front[0][1], cube.up[0][2]],
@@ -152,9 +152,9 @@ const cubeReducer = (cube: Cube, action: { type: Move }) => {
           [cube.down[2][0], cube.down[2][1], cube.front[2][2]],
         ],
         back: [
-          [cube.back[0][0], cube.back[0][1], cube.down[0][2]],
-          [cube.back[1][0], cube.back[1][1], cube.down[1][2]],
-          [cube.back[2][0], cube.back[2][1], cube.down[2][2]],
+          [cube.down[2][2], cube.back[0][1], cube.back[0][2]],
+          [cube.down[1][2], cube.back[1][1], cube.back[1][2]],
+          [cube.down[0][2], cube.back[2][1], cube.back[2][2]],
         ],
       };
     case 'U':
